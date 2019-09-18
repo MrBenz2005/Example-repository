01e89922b69b4e4e7634250d44514d7f8dc85b22
MrBenz <dkostya05@gmail.com>
2019-09-18 18:05:21 +0300

    Update test_kostya_derebensky8T.py

--- test_kostya_derebensky8T.py
+++ test_kostya_derebensky8T.py
@@ -1,5 +1,5 @@
 from kostya_derebensky8T1 import MyStack
-import pytest
+
 def test_add():
     matric = [[1, 2, 3, 4], [5, 6, 7, 8]]
     matric1 = [[9, 10, 11, 12], [13, 14, 15, 16]]
@@ -11,5 +11,5 @@ def test_sub():
     matric = [[1, 2, 3, 4], [5, 6, 7, 8]]
     matric1 = [[9, 10, 11, 12], [13, 14, 15, 16]]
     stack = MyStack()
-    assert(stack.__add__(matric,matric1) == [[10, 12, 14, 16], [18, 20, 22, 24]])
-test_add()
+    assert(stack.__sub__(matric,matric1) == [[10, 12, 14, 16], [18, 20, 22, 24]])
+test_sub()
