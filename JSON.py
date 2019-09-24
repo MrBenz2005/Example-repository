13515db2b81d01f2a615c0683678a5ed99bb7dad
MrBenz <dkostya05@gmail.com>
2019-09-24 19:45:38 +0300

    Update JSON.py

---
+++ JSON.py
@@ -0,0 +1,17 @@
+import json
+
+strange_dict = {
+    0: False,
+    5: "5",
+    "6": "Шесть",
+    7: None,
+    "eight": 8.0,
+    9.0: "nine",
+    "another_dict": {
+        0: True,
+        1: False,
+        2: None,
+        "one": "ONE",
+    }
+}
+print(json.dumps(strange_dict,  indent=4))
