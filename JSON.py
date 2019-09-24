50153534ba79be919057fc58fb25f1d04ea314de
MrBenz <dkostya05@gmail.com>
2019-09-24 21:50:19 +0300

    Update JSON.py

--- JSON.py
+++ JSON.py
@@ -14,5 +14,22 @@ strange_dict = {
         "one": "ONE",
     }
 }
-print(json.dumps(strange_dict, ensure_ascii=False, indent=4)) # Задание 1.1
+with open('strange_dict.json', 'w') as f_write:
+    json.dump(strange_dict, f_write, ensure_ascii=False, indent=4)
+# Задание 1.1 и 1.2
+unordered_dict = {
+    5: "Пять",
+    6: "Шесть",
+    9: "Девять",
+    4: "Четыре",
+    0: "Ноль",
+    7: "Семь",
+    8: "Восемь",
+    42: "Сорок два",
+    2: "Два",
+    1: "Один",
+    3: "Три",
+}
+with open('unordered_dict.json', 'w') as v_write:
+    json.dump(strange_dict, v_write, ensure_ascii=False, indent=4, sort_keys=True)
 
