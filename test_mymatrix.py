210ef44aaf71d8f48b621ae30a8780542e62cae2
MrBenz <dkostya05@gmail.com>
2019-09-24 19:38:59 +0300

    Update test_mymatrix.py

--- test_mymatrix.py
+++ test_mymatrix.py
@@ -1,4 +1,5 @@
 from klass_mymatrix import MyMatrix
+import pytest
 
 def test_add():
     matric = [[1, 2, 3, 4], [5, 6, 7, 8]]
@@ -6,11 +7,8 @@ def test_add():
     matrix = MyMatrix(matric1)
     matric3 = matrix + matric
     assert(matric3 == '10 12 14 16\n18 20 22 24')
-    try:
-        matrix = MyMatrix([])
-        matric3 = matrix + matric
-    except IndexError:
-        print('То что я думал')
+
+
 
 def test_sub():
     matric = [[1, 2, 3, 4], [5, 6, 7, 8]]
@@ -18,8 +16,4 @@ def test_sub():
     matrix = MyMatrix(matric)
     matric3 = matrix - matric1
     assert(matric3 == '0 0 0 0\n0 0 0 0')
-    try:
-        matrix = MyMatrix([])
-        matric3 = matrix - matric
-    except IndexError:
-        print('То что я думал')
+
