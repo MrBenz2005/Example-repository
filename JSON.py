1939a1038c1b0caeff7ce00f089d79320bf38eb4
MrBenz <dkostya05@gmail.com>
2019-09-25 11:21:13 +0300

    Update JSON.py

--- JSON.py
+++ JSON.py
@@ -30,8 +30,51 @@ unordered_dict = {
     1: "Один",
     3: "Три",
 }
-with open('unordered_dict.json', 'w') as v_write:
+with open('unordered_dict.json', 'w') as v_write:# Задание 1.4
     json.dump(unordered_dict, v_write, ensure_ascii=False, indent=4, sort_keys=True)
-with open('timetable_monday.json', 'r') as f_read:
+with open('timetable_monday.json', 'r') as f_read: # Задание 2.1
   timetable_dict = json.load(f_read)
 print(timetable_dict['8l'])
+
+"""
+Модуль json использует эту таблицу преобразования
++---------------+-----------+
+|     JSON      |  Python   |
++---------------+-----------+
+| object        | dict      |
+| array         | list      |
+| string        | unicode   |
+| number (int)  | int, long |
+| number (real) | float     |
+| true          | True      |
+| false         | False     |
+| null          | None      |
++---------------+-----------+
+
+в словарях ключи могут быть только одного формата
+
+"""
+strange_dict = {
+    5: "Пять",
+    6: "Шесть",
+    9: "Девять",
+    4: "Четыре",
+    0: "Ноль",
+    7: "Семь",
+    8: "Восемь",
+    42: "Сорок два",
+    2: "Два",
+    1: "Один",
+    3: "Три",
+}
+x = dict()
+y = None
+with open('y.json', 'w') as f_write:
+    strange = json.dump(y, f_write)
+print(strange)
+print(x)
+
+with open('strange_dict.json', 'w') as f_write:
+  dit = json.dump(strange_dict, f_write, sort_keys=True, ensure_ascii=False, indent=4)
+print(strange_dict)
+
