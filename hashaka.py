8c540071885ed004f8497862a84d05b89de2d458
MrBenz <dkostya05@gmail.com>
2020-01-29 11:36:16 +0300

    Update hashaka.py

--- hashaka.py
+++ hashaka.py
@@ -1,19 +1,25 @@
-class hash_table():
-    def __init__(self,hash):
-        spicok = []
-        for i in
-        self.hash = hash
+class My_class():
+    def __init__(self):
+        self.table = []
+        for i in range(50):
+            self.table.append([])
 
 
-    def add(self, object):
-
-        return self.hash
+    def add(self, value):
+        a = hash(value) % len(self.table)
+        if value in self.table[a]:
+            pass
+        else:
+            self.table[a].append(value)
 
     def remove(self, name):
-        del self.hash[name]
-        return self.hash
+        b = hash(name) % len(self.table)
+        if name in self.table[b]:
+            self.table[b].remove(name)
 
+
+"""
 spicok = [['kost', 'egort', 'mpops'], [18,14,43,3,7], ['tri', 'chetire', 'vosem']]
 Hashka = hash_table(spicok)
 print(Hashka.add('3', 'lada'))
-print(Hashka.remove('2'))
+print(Hashka.remove('2'))"""
