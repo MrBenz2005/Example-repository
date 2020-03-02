5dddb7e5255805c83b7efd032c2d19368df4e42e
MrBenz <dkostya05@gmail.com>
2020-03-02 11:35:24 +0300

    Update common_type.py

--- common_type.py
+++ common_type.py
@@ -1,12 +1,45 @@
 
 def get_common_type(type1: type, type2: type) -> type:
+    types = {1: bool, 2: int, 3: float, 4: complex, 5: range, 6: tuple, 7: list, 8: str}
+    """if type1 == str:
+        return str
+    elif type1 == list:
+        return list
+    elif type1 == float:
+        return float
+    elif type1 == complex:
+        return complex
+    elif type1 == range:
+        return range
+    elif type1 == tuple:
+        return tuple
+    elif type1 == bool:
+        return bool
+    elif type1 == int:
+        return int
 
-
-
-
-
-
-
+    if type2 == str:
+        return str
+    elif type2 == list:
+        return list
+    elif type2 == float:
+        return float
+    elif type2 == complex:
+        return complex
+    elif type2 == range:
+        return range
+    elif type2 == tuple:
+        return tuple
+    elif type2 == bool:
+        return bool
+    elif type2 == int:
+        return int
+"""
+    for key in types:
+        if type1 == key:
+            return key
+        if type2 == key:
+            return key
     """
     Calculate common type according to rule, that it must have the most adequate interpretation after conversion.
     Look in tests for adequacy calibration.
